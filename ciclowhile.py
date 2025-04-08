d846aa4d2739f7b68de9604b3b9bfc762f4a0c4b
SrKiller-lab <pavyelyo@gmail.com>
2025-04-08 14:14:31 -0600

    Update ciclowhile.py

--- ciclowhile.py
+++ ciclowhile.py
@@ -1,13 +1,15 @@
+#se le da el valor a la contraseña
 cont_corr = "quepasachaval"
 
+#se inicia un bucle
 while True:
     for i in range(5):
         cont_use = input("Dame la contraseña  ")
         if cont_corr == cont_use:
             print("Contraseña correcta")
-            break
+            break #se rompe el bucle cuando se pone la contraseña correcta
         print(f"Contraseña incorrecta, intentos,{i + 1}")
     else: 
         print("Contraseña incorrecta")
         print("Has alcanzado el limite de intentos")
-    break
+    break # se rompe el bucle cuando se te acaban los intentos
